fd2a276966b1e13cb77d577c904a9ca4d2de380c
Theo Stoffelbach <theo.stoffelbach@epitech.digital>
2022-11-23 21:03:14 +0100

    [ADD] Front-End start to setup with a Router (functional) And the first render of Home Page and the page 'Building' and start of back-end maybe delete with we are not utility of this.

---
+++ FrontEnd/src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import type {Component} from 'solid-js';
+
+const NAMEOFCOMPONENTS: Component = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default NAMEOFCOMPONENTS;
